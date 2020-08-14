1b7bd83ae4b2d4010d35ddb25e2a2fcfef9374a8
nk8713694 <nk8713694@gmail.com>
2020-08-14 09:19:52 +0530

    thapa technical

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+// ReactDOM.render(
+//   [
+//   <h1>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>,
+//   <p>like and subsribe me everywhere</p>,
+//   <p> and share it with your friends</p>
+//   ],
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//   <React.Fragment>
+//    <h1>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+//   <p>like and subsribe me everywhere</p>
+//   <p> and share it with your friends</p>
+
+//   </React.Fragment>
+  
+//   ,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <>
+     <h1>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+    <p>Top 5 net flix series</p>
+    <ol start="1">
+      <li>wear1 </li>
+      <li>wear2 </li>
+      <li>wear3 </li>
+      <li>wear4 </li>
+      <li>wear5 </li>
+    </ol>
+   
+  
+    </>
+    
+    ,
+    document.getElementById('root')
+  );
