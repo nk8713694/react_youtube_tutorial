d2b04d07d369a628ff17a2925ad90d35b7203d28
nk8713694 <nk8713694@gmail.com>
2020-08-14 20:52:51 +0530

    lec 14 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import "./index.css";
 
 // ReactDOM.render(
 //   [
@@ -23,21 +23,59 @@ import ReactDOM from 'react-dom';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(
-    <>
-     <h1>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
-    <p>Top 5 net flix series</p>
-    <ol start="1">
-      <li>wear1 </li>
-      <li>wear2 </li>
-      <li>wear3 </li>
-      <li>wear4 </li>
-      <li>wear5 </li>
-    </ol>
+// ReactDOM.render(
+//     <>
+//      <h1>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+//     <p>Top 5 net flix series</p>
+//     <ol start="1">
+//       <li>wear1 </li>
+//       <li>wear2 </li>
+//       <li>wear3 </li>
+//       <li>wear4 </li>
+//       <li>wear5 </li>
+//     </ol>
    
   
-    </>
+//     </>
     
-    ,
-    document.getElementById('root')
-  );
+//     ,
+//     document.getElementById('root')
+//   );
+
+var day = new Date().toLocaleDateString();
+var  time = new Date().toLocaleTimeString();
+  // ReactDOM.render(
+  //   <>
+  //    <h1 contentEditable="true">kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+  //   <p>Top 5 net flix series</p>
+      
+  //    <h1> {` ${day}` } </h1>
+  //    <h1> {` ${time}` } </h1>
+  
+  //   </>
+    
+  //   ,
+  //   document.getElementById('root')
+  // );
+
+
+
+
+  ReactDOM.render(
+      <>
+       <h1 className="heading ">kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+      <p>Top 5 net flix series</p>
+       
+       <div class="img-div">
+        <img src="https://picsum.photos/200/300"/>
+        <img src="https://picsum.photos/200/300"/>
+        <img src="https://picsum.photos/200/300"/>
+
+
+       </div>
+      </>
+      
+      ,
+      document.getElementById('root')
+    );
+
