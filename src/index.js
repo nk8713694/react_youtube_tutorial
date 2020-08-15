03ce40adfd59dd71bc9aac5e85abb2b787a18510
nk8713694 <nk8713694@gmail.com>
2020-08-15 06:56:09 +0530

    greeting app by myself

--- src/index.js
+++ src/index.js
@@ -61,27 +61,77 @@ var  time = new Date().toLocaleTimeString();
 
   // inline css
 
-  const heading ={
-    color:"red",
-    textTransform:'capitalize'
-  }
+  // const heading ={
+  //   color:"red",
+  //   textTransform:'capitalize'
+  // }
 
 
-  ReactDOM.render(
-      <>
-       <h1 style={{ color:"red", textTransform:'capitalize', textAlign:"center" }}>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
-      <p className="heading1">Top 5 net flix series</p>
+  // ReactDOM.render(
+  //     <>
+  //      <h1 style={{ color:"red", textTransform:'capitalize', textAlign:"center" }}>kya dikhana hai ... 2nd argument kaha dikhana hai </h1>
+  //     <p className="heading1">Top 5 net flix series</p>
        
-       <div class="img-div">
-        <img src="https://i.picsum.photos/id/465/536/354.jpg?hmac=WEXd5GqsLQ1Wiw0QZw0SEHS4wVLetFejAcyF-jkNH3A"/>
-        <img src="https://i.picsum.photos/id/665/536/354.jpg?hmac=ZUP396BZVlcimGikLaoXdSIZnc1itqxDyMOlOJv84hU"/>
-        <img src="https://picsum.photos/536/354"/>
+  //      <div class="img-div">
+  //       <img src="https://i.picsum.photos/id/465/536/354.jpg?hmac=WEXd5GqsLQ1Wiw0QZw0SEHS4wVLetFejAcyF-jkNH3A"/>
+  //       <img src="https://i.picsum.photos/id/665/536/354.jpg?hmac=ZUP396BZVlcimGikLaoXdSIZnc1itqxDyMOlOJv84hU"/>
+  //       <img src="https://picsum.photos/536/354"/>
 
 
-       </div>
+  //      </div>
+  //     </>
+      
+  //     ,
+  //     document.getElementById('root')
+  //   );
+
+  
+  var time= new Date().toLocaleTimeString().split(':');
+
+  var x= time[2].split(" ")
+  var hr=parseInt(time[0])
+  var ampm=x[1]
+
+const head1={
+  display:"inline-block",
+  textAlign:"center"
+}
+    const heading = {
+      display:"inline-block" ,
+      textAlign:"center"
+    }
+  var ans;
+  if((hr>=1 && hr<=11) && ampm=="AM" )
+  {
+    ans="Good Morning"
+    heading.color="red"
+  }
+  else if((hr>=1 && hr<7)&& ampm=="PM")
+  {
+     ans="good afternoon"
+     heading.color="blue"
+  }
+  else if((hr>=7 && hr<=11) && ampm=="PM")
+  {
+    ans="good night"
+    heading.color="pink"
+  }
+    ReactDOM.render(
+      <>
+     
+        
+
+        <h1 style={head1}>hello nitish</h1> <h1 style={heading}> {`${ans}`}</h1>
+       
+        
+       
       </>
       
       ,
       document.getElementById('root')
     );
 
+
+
+
+    
